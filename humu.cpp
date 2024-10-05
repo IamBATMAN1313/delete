5856cddb8591832904f701fec9aeced3fc131001
Muttakin <muttakin@Muttakins-MacBook-Air.local>
2024-10-05 21:50:54 +0600

    Siiiuuuu

---
+++ humu.cpp
@@ -0,0 +1,32 @@
+// [ ﷽ ] Bismillahir Rahmanir Raheem//
+#include <bits/stdc++.h>
+using namespace std;
+#define test int t; cin >> t; while(t--)
+#define M int m; cin >> m;
+#define N int n; cin >> n;
+#define ll long long
+#define f(i,a,b) for(int i=int(a);i<int(b);i++)
+#define fr(i,a,b) for(int i =int(a);i>int(b); i--)
+#define A(n) vector<int> a(n); f(i,0,n) cin >> a[i];
+#define pb push_back
+#define bg begin()
+#define ed end()
+#define sz size()
+#define pii pair<int,int>
+#define vi vector<int>
+#define mii map<int,int>
+#define vpi vector<pair<int,int>>
+#define SORT(x) sort(x.begin(),x.end())
+#define ALL(x) (x.begin(), x.end())
+#define YY cout << "YES" << endl;
+#define NN cout << "NO" << endl;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define warida return
+
+const int mod=1e9+7;
+const int INF = 2147483647;
+
+int main(){
+    cout << INTMAX_MAX +1;
+
+}
